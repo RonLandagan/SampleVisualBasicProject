d967a796c135503f2f31a667b45979625bec3a99
RonLandagan <51063654+RonLandagan@users.noreply.github.com>
2020-01-25 10:16:39 -0800

    Add files via upload

---
+++ PIIForm.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PIIForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RecordsToGenerateLabel = New System.Windows.Forms.Label()
+        Me.RecordsToGenerateTextBox = New System.Windows.Forms.TextBox()
+        Me.RecordsListBox = New System.Windows.Forms.ListBox()
+        Me.GenerateButton = New System.Windows.Forms.Button()
+        Me.VerifyPhoneButton = New System.Windows.Forms.Button()
+        Me.PrintButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'RecordsToGenerateLabel
+        '
+        Me.RecordsToGenerateLabel.AutoSize = True
+        Me.RecordsToGenerateLabel.Location = New System.Drawing.Point(20, 44)
+        Me.RecordsToGenerateLabel.Name = "RecordsToGenerateLabel"
+        Me.RecordsToGenerateLabel.Size = New System.Drawing.Size(122, 13)
+        Me.RecordsToGenerateLabel.TabIndex = 0
+        Me.RecordsToGenerateLabel.Text = "PII Records to Generate"
+        '
+        'RecordsToGenerateTextBox
+        '
+        Me.RecordsToGenerateTextBox.Location = New System.Drawing.Point(180, 41)
+        Me.RecordsToGenerateTextBox.Name = "RecordsToGenerateTextBox"
+        Me.RecordsToGenerateTextBox.Size = New System.Drawing.Size(61, 20)
+        Me.RecordsToGenerateTextBox.TabIndex = 1
+        '
+        'RecordsListBox
+        '
+        Me.RecordsListBox.FormattingEnabled = True
+        Me.RecordsListBox.Location = New System.Drawing.Point(23, 84)
+        Me.RecordsListBox.Name = "RecordsListBox"
+        Me.RecordsListBox.Size = New System.Drawing.Size(300, 147)
+        Me.RecordsListBox.TabIndex = 2
+        '
+        'GenerateButton
+        '
+        Me.GenerateButton.Location = New System.Drawing.Point(23, 250)
+        Me.GenerateButton.Name = "GenerateButton"
+        Me.GenerateButton.Size = New System.Drawing.Size(75, 23)
+        Me.GenerateButton.TabIndex = 3
+        Me.GenerateButton.Text = "Generate"
+        Me.GenerateButton.UseVisualStyleBackColor = True
+        '
+        'VerifyPhoneButton
+        '
+        Me.VerifyPhoneButton.Location = New System.Drawing.Point(104, 250)
+        Me.VerifyPhoneButton.Name = "VerifyPhoneButton"
+        Me.VerifyPhoneButton.Size = New System.Drawing.Size(75, 23)
+        Me.VerifyPhoneButton.TabIndex = 4
+        Me.VerifyPhoneButton.Text = "Verify Phone"
+        Me.VerifyPhoneButton.UseVisualStyleBackColor = True
+        '
+        'PrintButton
+        '
+        Me.PrintButton.Location = New System.Drawing.Point(185, 250)
+        Me.PrintButton.Name = "PrintButton"
+        Me.PrintButton.Size = New System.Drawing.Size(75, 23)
+        Me.PrintButton.TabIndex = 5
+        Me.PrintButton.Text = "Print"
+        Me.PrintButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(266, 250)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 23)
+        Me.ExitButton.TabIndex = 6
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'PIIForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(399, 295)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.PrintButton)
+        Me.Controls.Add(Me.VerifyPhoneButton)
+        Me.Controls.Add(Me.GenerateButton)
+        Me.Controls.Add(Me.RecordsListBox)
+        Me.Controls.Add(Me.RecordsToGenerateTextBox)
+        Me.Controls.Add(Me.RecordsToGenerateLabel)
+        Me.Name = "PIIForm"
+        Me.Text = "Personally Identifiable Information"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RecordsToGenerateLabel As Label
+    Friend WithEvents RecordsToGenerateTextBox As TextBox
+    Friend WithEvents RecordsListBox As ListBox
+    Friend WithEvents GenerateButton As Button
+    Friend WithEvents VerifyPhoneButton As Button
+    Friend WithEvents PrintButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ PhoneCheckForm.designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class PhoneCheckForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.PhoneNumberMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
+        Me.PhoneNumberLabel = New System.Windows.Forms.Label()
+        Me.VerifyButton = New System.Windows.Forms.Button()
+        Me.CloseButton = New System.Windows.Forms.Button()
+        Me.ResultStatusStrip = New System.Windows.Forms.StatusStrip()
+        Me.ResultStatusStripLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ResultStatusStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PhoneNumberMaskedTextBox
+        '
+        Me.PhoneNumberMaskedTextBox.Location = New System.Drawing.Point(210, 64)
+        Me.PhoneNumberMaskedTextBox.Mask = "(999) 000-0000"
+        Me.PhoneNumberMaskedTextBox.Name = "PhoneNumberMaskedTextBox"
+        Me.PhoneNumberMaskedTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.PhoneNumberMaskedTextBox.TabIndex = 1
+        '
+        'PhoneNumberLabel
+        '
+        Me.PhoneNumberLabel.AutoSize = True
+        Me.PhoneNumberLabel.Location = New System.Drawing.Point(198, 29)
+        Me.PhoneNumberLabel.Name = "PhoneNumberLabel"
+        Me.PhoneNumberLabel.Size = New System.Drawing.Size(123, 13)
+        Me.PhoneNumberLabel.TabIndex = 2
+        Me.PhoneNumberLabel.Text = "Phone Number To Verify"
+        '
+        'VerifyButton
+        '
+        Me.VerifyButton.Location = New System.Drawing.Point(167, 106)
+        Me.VerifyButton.Name = "VerifyButton"
+        Me.VerifyButton.Size = New System.Drawing.Size(75, 23)
+        Me.VerifyButton.TabIndex = 3
+        Me.VerifyButton.Text = "Verify"
+        Me.VerifyButton.UseVisualStyleBackColor = True
+        '
+        'CloseButton
+        '
+        Me.CloseButton.Location = New System.Drawing.Point(275, 106)
+        Me.CloseButton.Name = "CloseButton"
+        Me.CloseButton.Size = New System.Drawing.Size(75, 23)
+        Me.CloseButton.TabIndex = 4
+        Me.CloseButton.Text = "Close"
+        Me.CloseButton.UseVisualStyleBackColor = True
+        '
+        'ResultStatusStrip
+        '
+        Me.ResultStatusStrip.BackColor = System.Drawing.SystemColors.Control
+        Me.ResultStatusStrip.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.ResultStatusStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ResultStatusStripLabel})
+        Me.ResultStatusStrip.Location = New System.Drawing.Point(0, 164)
+        Me.ResultStatusStrip.Name = "ResultStatusStrip"
+        Me.ResultStatusStrip.Size = New System.Drawing.Size(508, 22)
+        Me.ResultStatusStrip.TabIndex = 5
+        Me.ResultStatusStrip.Text = "StatusStrip1"
+        '
+        'ResultStatusStripLabel
+        '
+        Me.ResultStatusStripLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ResultStatusStripLabel.Name = "ResultStatusStripLabel"
+        Me.ResultStatusStripLabel.Size = New System.Drawing.Size(121, 17)
+        Me.ResultStatusStripLabel.Text = "ToolStripStatusLabel1"
+        '
+        'PhoneCheckForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(508, 186)
+        Me.Controls.Add(Me.ResultStatusStrip)
+        Me.Controls.Add(Me.CloseButton)
+        Me.Controls.Add(Me.VerifyButton)
+        Me.Controls.Add(Me.PhoneNumberLabel)
+        Me.Controls.Add(Me.PhoneNumberMaskedTextBox)
+        Me.Name = "PhoneCheckForm"
+        Me.Text = "Form1"
+        Me.ResultStatusStrip.ResumeLayout(False)
+        Me.ResultStatusStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PhoneNumberMaskedTextBox As MaskedTextBox
+    Friend WithEvents PhoneNumberLabel As Label
+    Friend WithEvents VerifyButton As Button
+    Friend WithEvents CloseButton As Button
+    Friend WithEvents ResultStatusStrip As StatusStrip
+    Friend WithEvents ResultStatusStripLabel As ToolStripStatusLabel
+End Class
